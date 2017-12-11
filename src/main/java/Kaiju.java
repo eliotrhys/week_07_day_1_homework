7337e28280ce866914f73f9ecaf04d971bfd48b5
eliotrhys <eliot@eliotshort.co.uk>
2017-12-11 18:16:02 +0000

    initial commit ye

---
+++ src/main/java/Kaiju.java
@@ -0,0 +1,13 @@
+public abstract class Kaiju {
+
+    String name;
+    int healthValue;
+    int attackValue;
+
+    public Kaiju(){
+        this.name = name;
+        this.healthValue = healthValue;
+        this.attackValue = attackValue;
+    }
+
+}
